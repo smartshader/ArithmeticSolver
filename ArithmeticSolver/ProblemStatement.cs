b4f4300b258113ca3b33f065327a34b8ab1c6e4e
Imad Jerra <smartshader@gmail.com>
2021-07-03 18:51:35 -0400

    Implementing problem statement

---
+++ ArithmeticSolver/ProblemStatement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ArithmeticSolver
+{
+    internal class ProblemStatement
+    {
+        private readonly List<int> _input;
+        private readonly int _desiredResult;
+
+        public ProblemStatement(List<int> input, int desiredResult)
+        {
+            _input = input;
+            _desiredResult = desiredResult;
+        }
+    }
+}
--- ArithmeticSolver/Program.cs
+++ ArithmeticSolver/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ArithmeticSolver
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ProblemStatement problem = ReadProblem();
+        }
+
+        private static ProblemStatement ReadProblem()
+        {
+            Console.Write("Numbers to use (RETURN to exit): ");
+            string line = Console.ReadLine() ?? string.Empty;
+            string[] values = line.Split(new[] {" ", "\t"}, StringSplitOptions.RemoveEmptyEntries);
+
+            List<int> input = values
+                .Where(value => Regex.IsMatch(value, @"^\d+$"))
+                .Select(int.Parse)
+                .ToList();
+
+            if (input.Count == 0)
+                return null;
+            
+            Console.Write("           Enter desired result: ");
+            string rawNumber = Console.ReadLine() ?? string.Empty;
+            if (!int.TryParse(rawNumber, out int desiredResult))
+                desiredResult = 0;
+            
+            return new ProblemStatement(input, desiredResult);
         }
     }
 }
