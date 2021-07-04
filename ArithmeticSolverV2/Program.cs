2626f173b8a96c53fba39282964343cdaa2fa6eb
Imad Jerra <smartshader@gmail.com>
2021-07-03 21:20:00 -0400

    Create new Arithmetic solver v2

---
+++ ArithmeticSolverV2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArithmeticSolverV2
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
